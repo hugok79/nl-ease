21d7e86e10c3ab5b96b1739b1847af0a39475f65
diekrz2 <162576315+diekrz2@users.noreply.github.com>
2026-06-11 12:12:07 +0200

    code polishing and cppcheck

--- src/ui.c
+++ src/ui.c
@@ -1,6 +1,7 @@
 #include <Elementary.h>
 #include <libintl.h>
 #include <unistd.h>
+#include <Ecore.h>          
 #include "logic.h"
 
 #define _(String) gettext(String)
@@ -8,7 +9,8 @@
 static Evas_Object *start_spinner;
 static Evas_Object *end_spinner;
 
-static void kill_daemon(void)
+static void
+kill_daemon(void)
 {
     char cmd[256];
     pid_t my_pid = getpid();
@@ -19,6 +21,7 @@ static void kill_daemon(void)
 
     system(cmd);
     usleep(150000);  // 150ms
+
     snprintf(cmd, sizeof(cmd),
              "pgrep -f 'nl-ease --daemon' | grep -v %d | xargs -r kill -KILL 2>/dev/null || true", 
              my_pid);
@@ -28,7 +31,7 @@ static void kill_daemon(void)
 }
 
 static void
-on_toggle_changed(void *data, Evas_Object *obj, void *event_info)
+on_toggle_changed(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
 {
     int enabled = elm_check_state_get(obj);
     
@@ -43,15 +46,15 @@ on_toggle_changed(void *data, Evas_Object *obj, void *event_info)
 }
 
 static void
-on_slider_changed(void *data, Evas_Object *obj, void *event_info)
+on_slider_changed(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
 {
-    int val = (int)elm_slider_value_get(obj);
-    int real_temperature = 9000 - val;
+    int visual_value = (int)elm_slider_value_get(obj);
+    int real_temperature = 9000 - visual_value;
     logic_set_temperature(real_temperature);
 }
 
 static void
-on_schedule_changed(void *data, Evas_Object *obj, void *event_info)
+on_schedule_changed(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
     int start = (int)elm_spinner_value_get(start_spinner);
     int end   = (int)elm_spinner_value_get(end_spinner);
@@ -60,15 +63,15 @@ on_schedule_changed(void *data, Evas_Object *obj, void *event_info)
 }
 
 static void
-on_save_clicked(void *data, Evas_Object *obj, void *event_info)
+on_save_clicked(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
     logic_save();
 }
 
 static void
-on_launch_daemon_clicked(void *data, Evas_Object *obj, void *event_info)
+on_launch_daemon_clicked(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-    // save first
+    // first time config save
     logic_save();
 
     // close window
@@ -76,16 +79,17 @@ on_launch_daemon_clicked(void *data, Evas_Object *obj, void *event_info)
     if (win)
         evas_object_del(win);
 
-    // launch daemon in background
+    // launch daemon
     system("nl-ease --daemon &");
 
+    printf("Daemon launched. Closing GUI...\n");
     elm_exit();
 }
 
 static void
-on_win_delete(void *data, Evas_Object *obj, void *event_info)
+on_win_delete(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-    AppState *s = logic_get_state();
+    const AppState *s = logic_get_state();
     
     if (!s->enabled) {
         printf("Closing GUI with Enabled=OFF -> killing daemon\n");
@@ -95,71 +99,70 @@ on_win_delete(void *data, Evas_Object *obj, void *event_info)
     elm_exit();
 }
 
-void ui_init(void)
+void
+ui_init(void)
 {
     Evas_Object *win = elm_win_util_standard_add("nl-ease", "nl-ease");
     elm_win_autodel_set(win, EINA_TRUE);
 
-    evas_object_resize(win, 300, 270);
-    evas_object_size_hint_min_set(win, 300, 270);
-    evas_object_size_hint_max_set(win, 300, 270);
+    evas_object_resize(win, 300, 280);
+    evas_object_size_hint_min_set(win, 300, 280);
+    evas_object_size_hint_max_set(win, 300, 280);
     
-    elm_win_size_base_set(win, 300, 270);
-	elm_win_size_step_set(win, 0, 0);
+    elm_win_size_base_set(win, 300, 280);
+    elm_win_size_step_set(win, 0, 0);
 
     Evas_Object *box = elm_box_add(win);
     evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     elm_win_resize_object_add(win, box);
-    elm_box_padding_set(box, 0, 4);
+    elm_box_padding_set(box, 0, 6);
     evas_object_show(box);
 
-    // Toggle
+    // Toggle Enabled
     Evas_Object *toggle = elm_check_add(win);
     elm_object_text_set(toggle, _("Enabled"));
     evas_object_smart_callback_add(toggle, "changed", on_toggle_changed, NULL);
     elm_box_pack_end(box, toggle);
     evas_object_show(toggle);
 
-    // Slider
+    // Temperature Slider 
     Evas_Object *slider = elm_slider_add(win);
     elm_slider_min_max_set(slider, 2500, 6500);
     elm_slider_value_set(slider, 4500);
     elm_object_text_set(slider, _("Temperature"));
     evas_object_smart_callback_add(slider, "changed", on_slider_changed, NULL);
-    elm_box_pack_end(box, slider);
     evas_object_size_hint_min_set(slider, 220, -1);
+    elm_box_pack_end(box, slider);
     evas_object_show(slider);
 
-	// Start label
-	Evas_Object *start_label = elm_label_add(win);
-	elm_object_text_set(start_label, _("Start time:"));
-	elm_box_pack_end(box, start_label);
-	evas_object_show(start_label);
+    // Start time
+    Evas_Object *start_label = elm_label_add(win);
+    elm_object_text_set(start_label, _("Start time:"));
+    elm_box_pack_end(box, start_label);
+    evas_object_show(start_label);
 
-	// Start spinner
-	start_spinner = elm_spinner_add(win);
-	elm_spinner_min_max_set(start_spinner, 0, 23);
-	elm_spinner_value_set(start_spinner, 22);
-	evas_object_smart_callback_add(start_spinner, "changed", on_schedule_changed, NULL);
-	elm_box_pack_end(box, start_spinner);
-	evas_object_show(start_spinner);
+    start_spinner = elm_spinner_add(win);
+    elm_spinner_min_max_set(start_spinner, 0, 23);
+    elm_spinner_value_set(start_spinner, 22);
+    evas_object_smart_callback_add(start_spinner, "changed", on_schedule_changed, NULL);
+    elm_box_pack_end(box, start_spinner);
+    evas_object_show(start_spinner);
 
-	// End label
-	Evas_Object *end_label = elm_label_add(win);
-	elm_object_text_set(end_label, _("End time:"));
-	elm_box_pack_end(box, end_label);
-	evas_object_show(end_label);
+    // End time
+    Evas_Object *end_label = elm_label_add(win);
+    elm_object_text_set(end_label, _("End time:"));
+    elm_box_pack_end(box, end_label);
+    evas_object_show(end_label);
 
-	// End spinner
-	end_spinner = elm_spinner_add(win);
-	elm_spinner_min_max_set(end_spinner, 0, 23);
-	elm_spinner_value_set(end_spinner, 6);
-	evas_object_smart_callback_add(end_spinner, "changed", on_schedule_changed, NULL);
-	elm_box_pack_end(box, end_spinner);
-	evas_object_show(end_spinner);
-	
-	// Buttons
-	Evas_Object *btn_box = elm_box_add(win);
+    end_spinner = elm_spinner_add(win);
+    elm_spinner_min_max_set(end_spinner, 0, 23);
+    elm_spinner_value_set(end_spinner, 6);
+    evas_object_smart_callback_add(end_spinner, "changed", on_schedule_changed, NULL);
+    elm_box_pack_end(box, end_spinner);
+    evas_object_show(end_spinner);
+
+    // Buttons container
+    Evas_Object *btn_box = elm_box_add(win);
     elm_box_horizontal_set(btn_box, EINA_FALSE);
     elm_box_homogeneous_set(btn_box, EINA_TRUE);
     elm_box_padding_set(btn_box, 0, 8);
@@ -167,29 +170,32 @@ void ui_init(void)
     elm_box_pack_end(box, btn_box);
     evas_object_show(btn_box);
 
-    // Save button
+    // Save Configuration
     Evas_Object *btn_save = elm_button_add(win);
     elm_object_text_set(btn_save, _("Save Configuration"));
     evas_object_smart_callback_add(btn_save, "clicked", on_save_clicked, NULL);
     elm_box_pack_end(btn_box, btn_save);
     evas_object_show(btn_save);
-    
-    // Close and launch daemon button
+
+    // Close and Launch Daemon
     Evas_Object *btn_daemon = elm_button_add(win);
     elm_object_text_set(btn_daemon, _("Close and Launch Daemon"));
-    evas_object_smart_callback_add(btn_daemon, "clicked", on_launch_daemon_clicked, win);  // passa win come data
+    evas_object_smart_callback_add(btn_daemon, "clicked", on_launch_daemon_clicked, win);
     elm_box_pack_end(btn_box, btn_daemon);
     evas_object_show(btn_daemon);
-	
-	// UI sync
-	AppState *s = logic_get_state();
-	elm_check_state_set(toggle, s->enabled);
-	int visual_value = 9000 - s->temperature;
-    elm_slider_value_set(slider, visual_value);
-	elm_spinner_value_set(start_spinner, s->start_hour);
-	elm_spinner_value_set(end_spinner, s->end_hour);
-	
-    evas_object_show(win);
+
+    // Sync UI with current state
+    AppState *s = logic_get_state();
+    elm_check_state_set(toggle, s->enabled);
     
+    int visual_value = 9000 - s->temperature;
+    elm_slider_value_set(slider, visual_value);
+    
+    elm_spinner_value_set(start_spinner, s->start_hour);
+    elm_spinner_value_set(end_spinner, s->end_hour);
+
+    // Handle window close with 'X'
     evas_object_smart_callback_add(win, "delete,request", on_win_delete, NULL);
+
+    evas_object_show(win);
 }
